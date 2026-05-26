f86b7a16e72719a42d867d8e275e9d2095196257
dxdxffgg99 <dxdxffgg99@gmail.com>
2026-05-26 20:45:44 +0900

    biiiiiiiiiiiiiiggggggggggg update

--- .config/waybar/gpu_info.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-INFO=$(nvidia-smi --query-gpu=utilization.gpu,utilization.memory,temperature.gpu,memory.used,memory.total --format=csv,noheader,nounits)
-
-IFS=', ' read -r GPU_UTIL MEM_UTIL TEMP MEM_USED MEM_TOTAL <<< "$INFO"
-
-printf '{"text": "󰢮  %s%% %s°C", "tooltip": "󰢮 Usage: %s%%\\n󰍛 Vram: %sMB / %sMB\\n Temp: %s°C\\n󰈐 FanSpeed: %s%%"}\n' \
-    "$GPU_UTIL" "$TEMP" "$GPU_UTIL" "$MEM_USED" "$MEM_TOTAL" "$TEMP"
